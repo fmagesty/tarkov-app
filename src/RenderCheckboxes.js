e4e6bcefa4e9c4acb58d0f1bf611e52e36bbdd9a
Felipe MMS <fmagesty@gmail.com>
2023-01-02 16:12:08 -0300

    fea: add checkbox options as separate data file

--- src/RenderCheckboxes.js
+++ src/RenderCheckboxes.js
@@ -1,20 +1,9 @@
 import React from 'react'
 import Checkbox from './Checkbox'
+import { checkboxesData } from './data/checkboxesData'
 
 const RenderCheckboxes = () => {
-  // transfer this to a separate data file
-  const optionList = [
-    { id: 0, label: 'profit' },
-    { id: 1, label: 'ammo' },
-    { id: 2, label: 'maps' },
-    { id: 3, label: 'items' },
-    { id: 4, label: 'traders' },
-    { id: 5, label: 'bosses' },
-    { id: 6, label: 'barter' },
-    { id: 7, label: 'hideout' },
-  ]
-
-  let checkbox = optionList.map((item) => (
+  let checkbox = checkboxesData.map((item) => (
     <Checkbox item={item} key={item.id} />
   ))
 
---
+++ src/data/checkboxesData.js
@@ -0,0 +1,10 @@
+export const checkboxesData = [
+  { id: 0, label: 'profit' },
+  { id: 1, label: 'ammo' },
+  { id: 2, label: 'maps' },
+  { id: 3, label: 'items' },
+  { id: 4, label: 'traders' },
+  { id: 5, label: 'bosses' },
+  { id: 6, label: 'barter' },
+  { id: 7, label: 'hideout' },
+]
